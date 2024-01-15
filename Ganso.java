0cdd3656503ddabd3334b0eca582a34b11148679
GabrielPerera <gabrielpereira0907@gmail.com>
2024-01-15 13:46:08 -0300

    Questão do Ganso

---
+++ Ganso.java
@@ -0,0 +1,8 @@
+public class Ganso {
+    public String cor;
+    public double altura;
+    public void Ganso(String cor, double altura){
+        this.cor = cor;
+        this.altura = altura;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+
+        Ganso ganso1 = new Ganso("Cinza", 8);
+
+        Ganso ganso2 = new Ganso();
+
+        ganso2.altura = 5;
+        ganso2.cor = "Branco";
+    }
+
+
+}
